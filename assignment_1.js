f00ebb23e5d4af68fea251eb6b672c04ac0ce7ca
kmh8405 <kmh.ssem8405@gmail.com>
2026-04-01 15:58:45 +0900

    upload assignment file

--- assignment_1.js
+++ assignment_1.js
@@ -1,1 +1,9 @@
 // 연산자와 조건문을 활용해 콘솔 계산기 만들기 실습
+
+// 사용자 입력 코드 추가
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
